3f1d6d7737b44e6450afb69928e41f60a08432a2
ekohn <ekohn@smu.edu>
2013-12-03 22:42:07 -0600

    Created CheckersAdapter to draw the checkerboard in the GridView

--- src/com/cse3345/f13/kohn/GameActivity.java
+++ src/com/cse3345/f13/kohn/GameActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.GridLayout;
+import android.widget.GridView;
 
 public class GameActivity extends Activity {
 
@@ -13,17 +13,8 @@ public class GameActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 		View v;
-		GridLayout board = (GridLayout) findViewById(R.id.gameboard);
-		for (int i = 0; i < 64; i++) {
-			v = new View(this);
-			v.setMinimumHeight(100);
-			v.setMinimumWidth(100);
-			if (i % 2 == 0)
-				v.setBackgroundColor(0x000000);
-			else
-				v.setBackgroundColor(0xFFFFFF);
-			board.addView(v);
-		}
+		GridView board = (GridView) findViewById(R.id.gameboard);
+		board.setAdapter(new CheckersAdapter(this));
 		
 	}
 
